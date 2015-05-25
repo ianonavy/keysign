5b4bec1544f988fbd744334ab4480cd424c480bb
Ian Adam Naval <ianonavy@gmail.com>
2015-05-24 23:18:23 -0700

    Add individual downloads and examples
    Examples show exactly what commands to run for exporting and signing
    keys in the command line.

--- server.py
+++ server.py
@@ -5,6 +5,8 @@ import zipfile
 import shlex
 import hashlib
 from werkzeug import secure_filename
+
+
 class Chdir:
     def __init__( self, newPath ):
         self.savedPath = os.getcwd()
@@ -25,19 +27,22 @@ def hashfile(filepath):
         f.close()
     return sha1.hexdigest()
 
+
 UPLOAD_FOLDER = 'uploads'
 UPLOAD_KEYS_FOLDER = 'keys'
 UPLOAD_ARCHIVE_FOLDER = 'signed_keys'
-ALLOWED_EXTENSIONS = set(['gpg', 'zip', 'asc', 'sig'])
+ALLOWED_EXTENSIONS = set(['asc'])
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 ARCHIVE = os.path.join(app.config['UPLOAD_FOLDER'], 'csc_keys.zip')
 ARCHIVE_ALL = os.path.join(app.config['UPLOAD_FOLDER'], 'csc_signed_keys.zip')
 
+
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
@@ -53,8 +58,13 @@ def upload_file():
             return redirect(url_for('upload_file'))
     archive_sum = hashfile(ARCHIVE) if os.path.exists(ARCHIVE) else ""
     signed_archive_sum = hashfile(ARCHIVE_ALL) if os.path.exists(ARCHIVE_ALL) else ""
-    return render_template('index.html', fingerprints=get_fingerprints(),
-        archive_sum=archive_sum, signed_archive_sum=signed_archive_sum)
+    return render_template(
+        'index.html',
+        fingerprints=get_fingerprints(),
+        ids=get_ids(),
+        archive_sum=archive_sum,
+        signed_archive_sum=signed_archive_sum)
+
 
 @app.route('/signed_keys', methods=['GET', 'POST'])
 def upload_signed_keys():
@@ -75,17 +85,40 @@ def upload_signed_keys():
             return redirect(url_for('upload_file'))
     return redirect(url_for('upload_file'))
 
+
 def get_fingerprints():
     html = '<table class="table">'
+    html += '<tr><th>Filename</th><th>User ID</th><th>Key Fingerprint</th></tr>'
     with Chdir(app.config['UPLOAD_FOLDER']):
         for file_name in os.listdir(UPLOAD_KEYS_FOLDER):
             full_path = os.path.join(UPLOAD_KEYS_FOLDER, file_name)
             if '.gitignore' != file_name:
                 fingerprint = check_output(shlex.split('gpg --with-fingerprint ' + full_path)).decode('utf-8')
+                id_ = fingerprint.split('\n')[0].split('/')[1][:8]
                 fingerprint = fingerprint.split('\n')[1].split(' = ')[1]
-                html += "<tr><td>{}</td><td>{}</td></tr>".format(file_name, fingerprint)
+                url = url_for('download_key', filename=file_name)
+                file_name = "<a href='{}'>{}</a>".format(url, file_name)
+                html += "<tr><td>{}</td><td>{}</td><td>{}</td></tr>".format(file_name, id_, fingerprint)
     html += '</table>'
     return html
+
+
+def get_ids():
+    ids = []
+    with Chdir(app.config['UPLOAD_FOLDER']):
+        for file_name in os.listdir(UPLOAD_KEYS_FOLDER):
+            full_path = os.path.join(UPLOAD_KEYS_FOLDER, file_name)
+            if '.gitignore' != file_name:
+                fingerprint = check_output(shlex.split('gpg --with-fingerprint ' + full_path)).decode('utf-8')
+                id_ = fingerprint.split('\n')[0].split('/')[1][:8]
+                ids.append(id_)
+    return ids
+
+
+@app.route('/download/<filename>')
+def download_key(filename):
+    return send_file(filename, as_attachment=True)
+
 
 @app.route('/download')
 def download_keys():
